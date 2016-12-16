ec302a40f3b4ef20f1daec451f684c7524c21f1a
hrl7 <hrlclb@gmail.com>
2016-12-16 19:44:36 +0900

    chore(server): segeregate Johnny-five from index.js #1 #2

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 'use strict';
 const PORT = 4222;
 
-//iRobot create2 modules
-const SerialPort = require('serialport');
 const fs = require('fs');
 const debug = require('debug')('create2:driver');
-const Repl = require('repl');
 
 //Server settings
 const express = require('express');
@@ -19,199 +16,157 @@ http.listen(PORT, function(){
 });
 
 //Johnny-Five
-const five = require('johnny-five');
-const board = new five.Board({'repl':false});
+
 const BoardManager = require('./server/BoardManager.js');
-const boardManager = new BoardManager(io);
-
-//--HACK: Do I need to set these values in here?
-let led = null;
-let servo_yaw = 90;
-let servo_right = 90;
-let servo_left = 90;
-
-//--HACK: Tatsuki does not know how to set up these setting out of index.js
-board.on('ready', function() {
-  console.log('hello board');
-  led = new five.Led(13);
-  servo_yaw = new five.Servo({
-    id : 'yaw',
-    pin : 3,
-    range: [30, 150],
-    startAt: 90
-  });
-  servo_right = new five.Servo({
-    id : 'right',
-    pin : 6,
-    range: [30, 150],
-    startAt: 90
-  });
-  servo_left = new five.Servo({
-    id : 'left',
-    pin : 9,
-    range: [30, 150],
-    startAt: 90
-  });
-});
+const board = new BoardManager(io);
 
 //--HACK: This is fucking code. need refactoring ASAP!!!
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', (socket) => {
   console.log('hello socket');
-  socket.on('ledStatus', function(status) {
-    boardManager.ledStatus(led, status);
+  socket.on('ledStatus', (status) => {
+    board.changeLedStatus(status);
   });
-  socket.on('servo', function(data) {
+  socket.on('servo', (data) => {
     if (data.servo == 'servo_yaw') {
-      boardManager.servoMove(servo_yaw, data.vol);
+      board.servoMove('yaw', data.vol);
     } else if (data.servo == 'servo_right') {
-      boardManager.servoMove(servo_right, data.vol);
+      board.servoMove('right', data.vol);
     } else if (data.servo == 'servo_left') {
-      boardManager.servoMove(servo_left, data.vol);
+      board.servoMove('left', data.vol);
     }
   });
-  socket.on('speech', function(command) {
+  socket.on('speech', (command) => {
     if (command == 'name') {
-      servoAction(90, 10, 50, 1000);
+      board.servoAction(90, 10, 50, 1000);
       move0();
     } else if (command == 'morning') {
       console.log('command morning');
-      servoAction(80, 70, 50, 1000);
+      board.servoAction(80, 70, 50, 1000);
       move2();
       board.wait(3000, function() {
-        servoAction(120, 70, 80, 1000);
+        board.servoAction(120, 70, 80, 1000);
       });
     }
     else if (command == 'what') {
       console.log('command what');
-      servoAction(80, 70, 50, 1000);
+      board.servoAction(80, 70, 50, 1000);
       move1();
       board.wait(3000, function() {
-        servoAction(120, 70, 80, 1000);
+        board.servoAction(120, 70, 80, 1000);
       });
     } else if (command == 'who') {
       console.log('command what');
-      servoAction(90, 90, 90, 1000);
+      board.servoAction(90, 90, 90, 1000);
       move2();
       board.wait(3000, function() {
-        servoAction(90, 70, 80, 1000);
+        board.servoAction(90, 70, 80, 1000);
       });
     } else if (command == 'how-much') {
       console.log('command how-much');
-      servoAction(90, 10, 50, 1000);
+      board.servoAction(90, 10, 50, 1000);
       move2();
     } else if (command == 'business') {
       console.log('command business');
-      servoAction(90, 10, 50, 1000);
+      board.servoAction(90, 10, 50, 1000);
       move3();
       board.wait(3000, function() {
-        servoAction(90, 70, 80, 1000);
+        board.servoAction(90, 70, 80, 1000);
       });
     } else if (command == 'rich') {
       console.log('command rich');
-      servoAction(90, 90, 90, 1000);
+      board.servoAction(90, 90, 90, 1000);
       move4();
       board.wait(3000, function() {
-        servoAction(90, 90, 90, 1000);
+        board.servoAction(90, 90, 90, 1000);
       });
     } else if (command == 'danbo') {
       console.log('command danbo');
-      servoAction(30, 90, 90, 1000);
+      board.servoAction(30, 90, 90, 1000);
       move3();
       board.wait(3000, function() {
-        servoAction(90, 70, 80, 1000);
+        board.servoAction(90, 70, 80, 1000);
       });
     } else if (command == 'touch') {
       console.log('command touch');
-      servoAction(30, 90, 90, 1000);
+      board.servoAction(30, 90, 90, 1000);
       move1();
       board.wait(3000, function() {
-        servoAction(90, 70, 80, 1000);
+        board.servoAction(90, 70, 80, 1000);
       });
     } else if (command == 'weather') {
       console.log('command weather');
-      servoAction(10, 90, 90, 1000);
+      board.servoAction(10, 90, 90, 1000);
       move4();
       board.wait(4000, function() {
-        servoAction(10, 90, 80, 1000);
+        board.servoAction(10, 90, 80, 1000);
       });
     } else if (command == 'amazing') {
       console.log('command amazing');
-      servoAction(30, 90, 90, 1000);
+      board.servoAction(30, 90, 90, 1000);
       move4();
       board.wait(3000, function() {
-        servoAction(90, 70, 80, 1000);
+        board.servoAction(90, 70, 80, 1000);
       });
     }  else if (command == 'document') {
       console.log('command document');
-      servoAction(30, 90, 90, 1000);
+      board.servoAction(30, 90, 90, 1000);
       move1();
       board.wait(3000, function() {
-        servoAction(90, 70, 80, 1000);
+        board.servoAction(90, 70, 80, 1000);
       });
     } else if (command == 'noperson') {
       console.log('command noperson');
-      servoAction(10, 90, 90, 1000);
+      board.servoAction(10, 90, 90, 1000);
       move3();
       board.wait(4000, function() {
-        servoAction(10, 90, 80, 1000);
+        board.servoAction(10, 90, 80, 1000);
       });
     }
   });
 
 });
 
-function servoMove(servo, deg) {
-  console.log(servo.id + ':' + deg);
-  if (deg !== null && deg !== undefined) servo.to(deg);
-}
-
-function servoAction(yaw_d, right_d, left_d, movetime) {
-  servo_yaw.to(yaw_d, movetime);
-  servo_right.to(right_d, movetime);
-  servo_left.to(left_d, movetime);
-}
-
 function move0() {
   board.wait(1000, function() {
-    servoAction(90, 90, 90, 1000);
+    board.servoAction(90, 90, 90, 1000);
   });
   board.wait(2000, function() {
-    servoAction(10, 80, 100, 1000);
+    board.servoAction(10, 80, 100, 1000);
   });
 }
 
 function move1() {
   board.wait(1000, function() {
-    servoAction(60, 90, 90, 1000);
+    board.servoAction(60, 90, 90, 1000);
   });
   board.wait(2000, function() {
-    servoAction(80, 70, 50, 1000);
+    board.servoAction(80, 70, 50, 1000);
   });
 }
 
 function move2() {
   board.wait(1000, function() {
-    servoAction(100, 100, 100, 1000);
+    board.servoAction(100, 100, 100, 1000);
   });
   board.wait(2000, function() {
-    servoAction(90, 90, 70, 1000);
+    board.servoAction(90, 90, 70, 1000);
   });
 }
 
 function move3() {
   board.wait(1000, function() {
-    servoAction(20, 90, 90, 1000);
+    board.servoAction(20, 90, 90, 1000);
   });
   board.wait(2000, function() {
-    servoAction(120, 70, 50, 1000);
+    board.servoAction(120, 70, 50, 1000);
   });
 }
 
 function move4() {
   board.wait(1000, function() {
-    servoAction(10, 90, 90, 1000);
+    board.servoAction(10, 90, 90, 1000);
   });
   board.wait(2000, function() {
-    servoAction(150, 90, 90, 1000);
+    board.servoAction(150, 90, 90, 1000);
   });
 }
--- server/BoardManager.js
+++ server/BoardManager.js
@@ -1,40 +1,74 @@
 'use strict';
-
+const five = require('johnny-five');
+const board = new five.Board({'repl':false});
+const debug = require('debug')('warp:board');
 
 class BoardManager {
 
-  changeLedStatus(ledStatus) {
-    this.ledStatus = ledStatus;
-    this.io.sockets.emit('ledStatus', this.ledStatus);
+  constructor(io) {
+    this.io = io;
+    this.servos = {};
+    this.led = null;
+    this.isActive = false;
+    board.on('ready', function() {
+      this.led = new five.Led(13);
+      this.servos['yaw'] = new five.Servo({
+        id : 'yaw',
+        pin : 3,
+        range: [30, 150],
+        startAt: 90
+      });
+      this.servos['right'] = new five.Servo({
+        id : 'right',
+        pin : 6,
+        range: [30, 150],
+        startAt: 90
+      });
+      this.servos['left'] = new five.Servo({
+        id : 'left',
+        pin : 9,
+        range: [30, 150],
+        startAt: 90
+      });
+      this.isActive = true;
+    });
   }
 
-  ledStatus(led, status) {
-    this.led = led;
+  changeLedStatus(status) {
+    debug(`change LED status: ${status}`)
+    if(!this.isActive) return;
+    this.io.sockets.emit('ledStatus', this.ledStatus);
     this.status = status;
     if(status == 'on') {
       console.log('turn on led...');
-      led.on();
+      this.led.on();
     } else if (status == 'blink') {
       console.log('blink led...');
-      led.blink(500);
+      this.led.blink(500);
     } else {
       console.log('turn off led');
-      led.stop().off();
+      this.led.stop().off();
     }
   }
 
-  servoMove(servo, deg) {
-    console.log(servo.id + ':' + deg);
-    if (deg !== null && deg !== undefined) servo.to(deg);
+  servoMove(servoId, deg) {
+    debug(`servo move: ${servoId} to ${deg}`)
+
+    if(!this.isActive) return;
+    if (deg !== null && deg !== undefined) this.servos[servoId].to(deg);
   }
 
-  servoAction(yaw, yaw_d, right, right_d, left, left_d, movetime) {
-    yaw.to(yaw_d, movetime);
-    right.to(right_d, movetime);
-    left.to(left_d, movetime);
+  servoAction(yaw_d, right_d, left_d, movetime) {
+    debug(`servo action, yaw: ${yaw_d}, right: ${right_d}, left: ${left_d}`)
+
+    if(!this.isActive) return;
+    this.servos.yaw.to(yaw_d, movetime);
+    this.servos.right.to(right_d, movetime);
+    this.servos.left.to(left_d, movetime);
   }
 
   servoCommand(board, command, yaw, right, left) {
+    if(!this.isActive) return;
     let time = 0;
     if (command == 'name') {
       this.servoMove(yaw, 10);
@@ -46,30 +80,30 @@ class BoardManager {
         this.servoAction(yaw, 60, right, 90, left, 90, 1000);
       });
     }
-
-    /*switch (command) {
-      case "name":
-        console.log("command:" + command);
-        this.servoMove(yaw, 10);
-        this.servoAction(yaw, 10, right, 100, left, 90, 1000);
-        break;
-      case "what" :
-        console.log("command what");
-        this.servoAction(yaw, 80, right, 70, left, 50, 1000);
-        this.board.wait(time, function() {
-          this.servoAction(yaw, 60, right, 90, left, 90, 1000);
-        });
-        break;
-    }*/
   }
 
-
-
   servoStop(){
-    servo_yaw.stop();
-    servo_right.stop();
-    servo_left.stop();
+    debu('servo stop');
+    if(!this.isActive) return;
+
+    this.servos.yaw.stop();
+    this.servos.right.stop();
+    this.servos.left.stop();
     time = 0;
+  }
+
+  wait(time, callback) {
+    debug(`waiting ${time} ms, callback: ${callback}`);
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        debug('awake');
+        if(callback) {
+          debug(`do callback`);
+          callback();
+        }
+        res();
+      }, time);
+    });
   }
 }
 

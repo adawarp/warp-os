89927d437da671b1ca68fac0365acc4b18114c48
hrl7 <hrlclb@gmail.com>
2016-12-22 16:54:20 +0900

    feat(all): add electron and modulize server

---
+++ src/main.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+console.log(process.versions);
+
+const path = require('path');
+const url = require('url');
+const Server = require('./server/Server');
+
+let mainWindow;
+let server;
+
+function createWindow () {
+  server = new Server();
+  server.start();
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL(url.format({
+    pathname: 'localhost:4222',
+    protocol: 'http:',
+    slashes: true
+  }));
+
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});

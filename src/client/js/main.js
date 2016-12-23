62120fb1f41c75c9a30012ca716c0178da78fe17
hrl7 <hrlclb@gmail.com>
2016-12-23 18:34:23 +0900

    feat(client): add video chat

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -4,20 +4,24 @@
           initQuestionList: false,
           initLanguageList: false */
 
-let conn, peer = new Adawarp(), socket = io.connect();
+let conn, call, localStream, peer = new Adawarp(), socket = io.connect();
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
 window.addEventListener('DOMContentLoaded', function(){
 
-  peer.on('open', function(){
+  peer.on('open', function(info){
+    document.getElementById('my_id').textContent = info.id;
   });
   peer.login();
 
   initDeviceManager();
   initQuestionList();
   initLanguageList();
+  initLocalVideo();
 });
 
-peer.on('connection', function(conn) {
+peer.on('connection', function(_conn) {
+  conn = _conn;
   document.getElementById('partner_id').textContent = conn.peer;
   conn.on('data', function(data){
     document.getElementById('receive_message').textContent = data;
@@ -25,10 +29,57 @@ peer.on('connection', function(conn) {
   });
 });
 
+peer.on('call', function(_call){
+  if (!localStream) {
+    alert('permit user media');
+    return;
+  }
+  call = _call;
+  document.getElementById('partner_id').innerHTML = call.peer;
+  call.on('stream', function(stream){
+    const url = URL.createObjectURL(stream);
+    document.getElementById('remote_video').src = url;
+  });
+  call.answer(localStream);
+
+});
+
 function startCall(){
   let partnerId = document.getElementById('partner-id-input').value;
-  if (partnerId) {
-    conn = peer.connect(partnerId);
+  if (!partnerId) {
+    alert('type your partner id');
+    return;
+  }
+
+  if (!localStream) {
+    alert('permit user media');
+    return;
+  }
+
+  call = peer.call(partnerId, localStream);
+  call.on('stream', function(stream){
+    document.getElementById('partner_id').innerHTML = partnerId;
+    const url = URL.createObjectURL(stream);
+    document.getElementById('remote_video').src = url;
+  });
+}
+
+function endCall() {
+  if (conn) {
+    conn.close();
+    conn = null;
+  }
+  if (call) {
+    call.close();
+    call = null;
   }
 }
 
+function initLocalVideo() {
+  navigator.getUserMedia({audio: true, video: true}, function(stream){
+    localStream = stream;
+    document.getElementById('local_video').src = URL.createObjectURL(stream);
+  }, function() {
+    alert('Error!');
+  });
+}
